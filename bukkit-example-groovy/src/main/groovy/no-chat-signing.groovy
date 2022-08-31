31cbcadb30b7533ef0176c08f0e33521691bce53
PinkLolicorn <pinklolicorngithub@daffit.pl>
2022-08-31 11:22:25 +0200

    Add no-chat-signing.groovy example

---
+++ bukkit-example-groovy/src/main/groovy/no-chat-signing.groovy
@@ -0,0 +1,17 @@
+@BaseScript BukkitGroovyScript script
+
+import eu.okaeri.poly.api.bukkit.BukkitGroovyScript
+import groovy.transform.BaseScript
+import org.bukkit.event.EventPriority
+import org.bukkit.event.player.AsyncPlayerChatEvent
+
+/**
+ * Resends chat messages as system messages, thus removing
+ * "this message is not secure" warnings or any message
+ * reporting capabilities.
+ */
+listen(AsyncPlayerChatEvent, EventPriority.MONITOR, true) {
+    it.cancelled = true
+    def result = String.format(it.format, it.player.name, it.message)
+    it.recipients.each { it.sendMessage(result) }
+}
