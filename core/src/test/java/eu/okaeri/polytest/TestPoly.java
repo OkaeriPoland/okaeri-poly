17d2086092797f1de302c7ba50b7dbf2a56f5e4c
Sandra <sandra@okaeri.cloud>
2024-08-09 23:44:17 +0200

    Drop jython and graal support (might re-add as extensions someday) - Cuts the jar size by the factor of 10 (from 80MB to 8MB)

--- core/src/test/java/eu/okaeri/polytest/TestPoly.java
+++ core/src/test/java/eu/okaeri/polytest/TestPoly.java
@@ -2,35 +2,14 @@ package eu.okaeri.polytest;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
-import lombok.Cleanup;
-import lombok.SneakyThrows;
 import org.codehaus.groovy.control.CompilerConfiguration;
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Source;
-import org.graalvm.polyglot.Value;
 import org.junit.jupiter.api.Test;
-import org.python.core.PyObject;
-import org.python.util.PythonInterpreter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestPoly {
 
     private static final boolean DEBUG = Boolean.getBoolean("poly.debug");
-
-    @Test
-    @SneakyThrows
-    public void test_basic_graal_context() {
-
-        @Cleanup Context context = Context.newBuilder("js")
-            .option("engine.WarnInterpreterOnly", (DEBUG ? "true" : "false"))
-            .build();
-        context.eval(Source.newBuilder("js", "const a = 1 + 1", "src.js").build());
-        Value result = context.getBindings("js").getMember("a");
-
-        Integer integer = result.as(Integer.class);
-        assertEquals(2, integer);
-    }
 
     @Test
     public void test_basic_groovy_shell() {
@@ -42,17 +21,6 @@ public class TestPoly {
         Object result = shell.evaluate("1 + 1");
 
         Integer integer = (Integer) result;
-        assertEquals(2, integer);
-    }
-
-    @Test
-    @SneakyThrows
-    public void test_basic_jython() {
-
-        @Cleanup PythonInterpreter python = new PythonInterpreter();
-        PyObject result = python.eval("1 + 1");
-
-        int integer = result.asInt();
         assertEquals(2, integer);
     }
 }
--- dts-generator/mc1_18_2/src/test/java/Mc1_18_2Generator.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class Mc1_18_2Generator extends CommonBukkitGenerator {
-
-    @Test
-    public void generate() {
-        this.write("bukkit", "mc1.18.2", this.defaults());
-    }
-}
